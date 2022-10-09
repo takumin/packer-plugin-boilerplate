74d1d591cad40cc1e2ab568bc1a629027bb9c15c
takumin <takumiiinn@gmail.com>
2022-10-10 00:09:03 +0900

    merge config.go to builder.go

--- builder/example/builder.go
+++ builder/example/builder.go
@@ -1,13 +1,20 @@
+//go:generate packer-sdc mapstructure-to-hcl2 -type Config
+
 package example
 
 import (
 	"context"
 
 	"github.com/hashicorp/hcl/v2/hcldec"
+	"github.com/hashicorp/packer-plugin-sdk/common"
 	"github.com/hashicorp/packer-plugin-sdk/packer"
 )
 
 const BuilderId = "example.builder"
+
+type Config struct {
+	common.PackerConfig `mapstructure:",squash"`
+}
 
 type Builder struct {
 	config Config
